500f8698c0d9c8f6adf8b9b6caaae223b800bb74
Vivexa Tech <vivexatech@gmail.com>
2025-08-11 20:05:29 +0530

    Create kjdshfsufsch.js

---
+++ VivexaTech/kjdshfsufsch.js
@@ -0,0 +1,8 @@
+var firebaseConfig = {
+      apiKey: "AIzaSyDlmiQxH2ZFU2eUZ7V3JRtNNy8ozzJJ2_k",
+      authDomain: "vivexa-testimonial.firebaseapp.com",
+      projectId: "vivexa-testimonial",
+      storageBucket: "vivexa-testimonial.appspot.com",
+      messagingSenderId: "615030329099",
+      appId: "1:615030329099:web:a4021bf93cb8a48bd71c0e",
+};
